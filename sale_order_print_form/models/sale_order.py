f33f3fc966855af743e1c7eef3c667e2e1ef2d09
ievgen <synchyshyn.i@web.de>
2020-10-20 14:58:36 +0300

    [UPDATE] 13.0.1.0.9

--- sale_order_print_form/models/sale_order.py
+++ sale_order_print_form/models/sale_order.py
@@ -7,3 +7,16 @@ _logger = logging.getLogger(__name__)
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
+
+    so_pf_amount_prepaid = fields.Monetary(
+        string='Prepaid', compute='_compute_prepaid',
+        default=0, )
+
+    @api.depends('invoice_ids')
+    def _compute_prepaid(self):
+        for obj in self:
+            obj.so_pf_amount_prepaid = 0
+            paid_sum = \
+                sum(obj.invoice_ids.mapped('amount_total')) - sum(
+                    obj.invoice_ids.mapped('amount_residual'))
+            obj.so_pf_amount_prepaid = paid_sum
