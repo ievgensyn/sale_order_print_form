c976bf3cd8ad4ef08060211e84bd1e203340b604
ievgen <synchyshyn.i@web.de>
2020-10-20 21:37:46 +0300

    [UPDATE] 13.0.1.0.11

--- sale_order_print_form/__manifest__.py
+++ sale_order_print_form/__manifest__.py
@@ -8,7 +8,7 @@
 
     'category': 'Sales',
     'license': 'Other proprietary',
-    'version': '13.0.1.0.10',
+    'version': '13.0.1.0.11',
 
     'depends': [
         'base', 'sale_management', 'barcodes', 'account', 'sale_stock'
